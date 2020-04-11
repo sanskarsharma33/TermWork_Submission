dd180d04f25bfa548c8893a2a3c54c5db2120dae
Sanskar <47107112+sanskarsharma33@users.noreply.github.com>
2020-04-12 01:16:35 +0530

    Update urls.py

--- student_login/urls.py
+++ student_login/urls.py
@@ -4,10 +4,17 @@ from .models import UserDetails
 from rest_framework.generics import ListCreateAPIView
 from student_login.serializers import UserSerializer
 from student_login import views
+from django.contrib.auth.views import PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('loginpage/', views.Login, name='login'),
     path('auth/', views.auth_view, name='auth'),
-    path('registeration/', views.registerpage, name='register'),
+    path('change_profile/', views.change_profilepage, name='change_profile'),
+    path('change_password/',views.change_password,name='change_password'),
+    path('logout/',views.Logout,name='logout'),
+    path('password_reset/', PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset/complete/', PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
 ]
