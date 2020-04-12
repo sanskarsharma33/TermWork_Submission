bf7737c74efa1b812dbe877c86e47999af1df3df
Sanskar <47107112+sanskarsharma33@users.noreply.github.com>
2020-04-12 22:25:37 +0530

    Update views.py

--- student_login/views.py
+++ student_login/views.py
@@ -5,13 +5,16 @@ from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from django.shortcuts import render,redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.models import User
 from django.template.context_processors import csrf
 from . import forms
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+import json,io
 
 # Create your views here.
 
@@ -27,29 +30,34 @@ def auth_view(request):
     user=authenticate(username=usrname,password=passwd)
     if user is not None:
         login(request, user)
-        curr_user=UserDetails.objects.all()
-        print(curr_user.get(user=user).first_name)
-        if  curr_user.get(user=user).first_name != 'null' :
-            print(curr_user.get(user=user).first_name,curr_user.get(user=user).last_name,curr_user.get(user=user).email)
+        curr_user=UserDetails.objects.all().get(user = user)
+        if  curr_user.first_name != 'null' :
+            print(curr_user.first_name,curr_user.last_name,curr_user.email)
             return(HttpResponse("OK"))
         else :
-            return(redirect('/login/loginpage'))
+            return(redirect('/login/change_profile'))
     else:
         return(HttpResponse("Wrong Credentials"))
 
 @login_required(login_url='/login/loginpage/')
+@csrf_exempt
 def change_profilepage(request):
     c = {}
     c.update(csrf(request))
     if request.method=="POST":
-        pform=forms.ProfileForm(request.POST, instance=request.user)
-        if pform.is_valid():
-            curr_user=UserDetails.objects.all()
-            curr_user.get(user=request.user).set_details(request)
-            return redirect('/login/change_password')
+        try:
+            user = UserDetails.objects.all().get(user=request.user)
+        except UserDetails.DoesNotExist:
+            return HttpResponse(status=404)
+        serializer = UserSerializer(user, data=request.POST)
+        if serializer.is_valid():
+            serializer.save()
+        #return JsonResponse(serializer.data, status=201)
+        return redirect('/login/change_password/')
     else:
         pform=forms.ProfileForm()
-    return render(request,"createuser.html",{ 'pform': pform})
+    return render(request,"createuser.html",{'pform': pform})
+
 
 @login_required(login_url='/login/loginpage/')
 def change_password(request):
