11540499f32823f1eb5ee6f80e84872d76326073
Sanskar <47107112+sanskarsharma33@users.noreply.github.com>
2020-04-13 01:03:29 +0530

    Update serializers.py

--- student_login/serializers.py
+++ student_login/serializers.py
@@ -8,10 +8,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = UserDetails
         fields = ('user', 'first_name', 'last_name', 'email', 'role', 'year_of_intake')
 
-    def create(self, validated_data):
-        self.first_name = validated_data.get('first_name', self.first_name)
-        self.last_name = validated_data.get('last_name', self.last_name)
-        self.email = validated_data.get('email', self.email)
-        self.year_of_intake = validated_data.get('year_of_intake', self.year_of_intake)
-        self.save()
-        return self.user
+    """def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.year_of_intake = validated_data.get('year_of_intake', instance.year_of_intake)
+        instance.save()
+        return instance"""
