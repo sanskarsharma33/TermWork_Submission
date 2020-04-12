5a567bdfcab4f1d73fc1b5c93bbbd65969b1783e
Sanskar <47107112+sanskarsharma33@users.noreply.github.com>
2020-04-12 22:27:17 +0530

    Update settings.py

--- TermWork_Submission/settings.py
+++ TermWork_Submission/settings.py
@@ -126,3 +126,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+"""
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'whitehat111333@gmail.com'
+EMAIL_HOST_PASSWORD = 'ssylehowktsybekc'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+(Real time email setting)
+"""
