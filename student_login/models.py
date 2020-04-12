ff796b5759a61525593d92a2896c74e8f377c9c6
Sanskar <47107112+sanskarsharma33@users.noreply.github.com>
2020-04-12 22:26:35 +0530

    Update models.py

--- student_login/models.py
+++ student_login/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+# Create your models here.
+from django.contrib.auth.models import  User,Group
 import datetime
+from django.utils.translation import gettext_lazy as _
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
 def current_year():
     return datetime.date.today().year
 
@@ -13,19 +15,19 @@ def max_value_current_year(value):
     return MaxValueValidator(current_year())(value)
 
 class UserDetails(models.Model):
+    
+    class Role(models.TextChoices):
+        Student = 'ST', _('Student')
+        Faculty = 'FC', _('Faculty')
+
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     first_name=models.CharField(max_length=20, default='null')
     last_name=models.CharField(max_length=20, default='null')
     email=models.EmailField(max_length=50)
-    role=models.CharField(max_length=1)
+    role = models.CharField(max_length=2,
+        choices=Role.choices,
+        default=Role.Student)
     year_of_intake=models.PositiveIntegerField(default=current_year(), validators=[MinValueValidator(2017), max_value_current_year])
-
-    def set_details(self, request):
-        self.first_name=request.POST.get('first_name')
-        self.last_name=request.POST.get('last_name')
-        self.email=request.POST.get('email')
-        self.year_of_intake=request.POST.get('year_of_intake')
-        self.save()
 
 
 @receiver(post_save, sender=User)
